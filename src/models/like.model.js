bf78f82cb0212b55927e573a871bc4cb19dba2be
MehulCodr <mehulgupta0910@gmail.com>
2026-04-07 23:38:49 +0530

    Refactor logoutUser to set refreshToken to null and add models for Comment, Like, Playlist, and Tweet

---
+++ src/models/like.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema({
+    likedBy : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    video : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Video"
+    },
+
+    tweet : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Tweet"
+    },
+    comment : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Comment"
+    }
+},{timestamps : true})
+
+export const Like = mongoose.model("Like" , likeSchema)
