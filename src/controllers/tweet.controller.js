339b52a0e2a846b3260faed40e8c55d6828ad2bf
MehulCodr <mehulgupta0910@gmail.com>
2026-04-11 16:13:05 +0530

    Enhance tweet management: add validation and error handling for create, update, and delete operations

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,41 +1,114 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Tweet } from "../models/tweet.model.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
-    const {content} = req.body;
-    if(!content){
+    const { content } = req.body;
+    if (!content?.trim()) {
         throw new ApiError(400, "Content is required");
     }
 
-    const Tweet = await Tweet.create({
-        content : content,
-        author : req.user._id//comes from auth middleware
+    const tweet = await Tweet.create({
+        content: content,
+        author: req.user._id//comes from auth middleware
     })
 
     return res
-    .status(201)
-    .json(new ApiResponse(201, "Tweet created successfully", Tweet))
+        .status(201)
+        .json(new ApiResponse(201, "Tweet created successfully", tweet))
 
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    const {userId} = req.params;
-    if(!isValidObjectId(userId)){
+    const { userId } = req.params;
+    if (!isValidObjectId(userId)) { //comes from mongoose
         throw new ApiError(400, "Invalid user id");
     }
+
+    const user = await User.findById(userId);
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+    const tweets = await Tweet.find({ author: userId }).populate("author", "username fullname").sort({ createdAt: -1 });//-1 because we want the latest tweets first
+    //why aggregate not used here? because we don't have any complex operations to perform on the data, we just need to filter tweets by author and sort them by createdAt. Aggregate is more useful when we need to perform complex operations like grouping, joining, etc. which is not the case here.
+
+    //if aggregate is used it will look like this:
+    // const tweets = await Tweet.aggregate([
+    //     {$match : {author : new mongoose.Types.ObjectId(userId)}},
+    //     {$sort : {createdAt : -1}}
+    // ])
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "User tweets fetched successfully", tweets))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const { tweetId } = req.params;
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweet id");
+    }
+
+    // // alternate way to check if the id is valid or not
+    // if (!mongoose.Types.ObjectId.isValid(tweetId)) {
+    //     throw new ApiError(400, "Invalid tweet id");
+    // }
+
+    const { content } = req.body;
+    if (!content) {
+        throw new ApiError(400, "Content is required");
+    }
+    //check if same user is trying to update the tweet or not
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found");
+    }
+
+    if (tweet.author.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not the owner of this tweet");
+    }
+
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        { content: content },
+        { new: true }
+    );
+
+    if (!updatedTweet) {
+        throw new ApiError(404, "Tweet not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Tweet updated successfully", updatedTweet))
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const { tweetId } = req.params;
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id");
+    }
+
+    //check if same user is trying to delete the tweet or not
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found");
+    }
+
+    if (tweet.author.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not the owner of this tweet");
+    }
+
+    await Tweet.findByIdAndDelete(tweetId);
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Tweet deleted successfully", {}))
 })
 
 export {
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -14,4 +14,7 @@ router.route("/").post(createTweet);
 router.route("/user/:userId").get(getUserTweets);
 router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
 
+//why patch and delete chained together? because they both operate on the same resource (tweet) and it makes sense to group them together. It also makes the code cleaner and more organized. If we had separate routes for patch and delete, it would be more verbose and less intuitive.
+//how to know which will be called when we hit the endpoint? it depends on the HTTP method used in the request. If we send a PATCH request to /:tweetId, the updateTweet function will be called. If we send a DELETE request to /:tweetId, the deleteTweet function will be called. The router will automatically route the request to the correct handler based on the HTTP method used.
+
 export default router
